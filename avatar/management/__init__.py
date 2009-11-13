08a30aa851a773999f8e4468a9b02a5032301f9a
James Turnbull <admin@incuna.com>
2009-11-13 14:43:59 +0000

    Cleaned up noticetext, removing reference to the user's gender and nonsense regarding tribes

--- avatar/management/__init__.py
+++ avatar/management/__init__.py
@@ -6,8 +6,8 @@ if "notification" in settings.INSTALLED_APPS:
     from notification import models as notification
     
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("avatar_updated", _("Avatar Updated"), _("avatar have been updated"))
-        notification.create_notice_type("avatar_friend_updated", _("Friend Updated Avatar"), _("a friend has updated his avatar"))
+        notification.create_notice_type("avatar_updated", _("Avatar Updated"), _("your avatar has been updated"))
+        notification.create_notice_type("avatar_friend_updated", _("Friend Updated Avatar"), _("a friend has updated their avatar"))
     
     signals.post_syncdb.connect(create_notice_types, sender=notification)
 else:
