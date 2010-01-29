2104bbd71adc99623836493856cf4b51fa6ada82
Ross Poulton <ross@rossp.org>
2010-01-30 06:03:40 +0800

    Provide Gravatar default url in settings.AVATAR_GRAVATAR_DEFAULT - will be used by Gravatar if the user has no avatar for that email address.

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -11,6 +11,7 @@ AUTO_GENERATE_AVATAR_SIZES = getattr(settings, 'AUTO_GENERATE_AVATAR_SIZES', (80
 AVATAR_RESIZE_METHOD = getattr(settings, 'AVATAR_RESIZE_METHOD', Image.ANTIALIAS)
 AVATAR_STORAGE_DIR = getattr(settings, 'AVATAR_STORAGE_DIR', 'avatars')
 AVATAR_GRAVATAR_BACKUP = getattr(settings, 'AVATAR_GRAVATAR_BACKUP', True)
+AVATAR_GRAVATAR_DEFAULT = getattr(settings, 'AVATAR_GRAVATAR_DEFAULT', None)
 AVATAR_DEFAULT_URL = getattr(settings, 'AVATAR_DEFAULT_URL', 
     settings.MEDIA_URL + os.path.join(os.path.dirname(__file__), 'default.jpg'))
 
