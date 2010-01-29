073e9a9c10f8a158f13b56ef38addc49d05b43ff
Ross Poulton <ross@rossp.org>
2010-01-30 06:03:40 +0800

    Provide Gravatar default url in settings.AVATAR_GRAVATAR_DEFAULT - will be used by Gravatar if the user has no avatar for that email address.

--- avatar/templatetags/avatar_tags.py
+++ avatar/templatetags/avatar_tags.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from django.utils.hashcompat import md5_constructor
 
-from avatar import AVATAR_DEFAULT_URL, AVATAR_GRAVATAR_BACKUP
+from avatar import AVATAR_DEFAULT_URL, AVATAR_GRAVATAR_BACKUP, AVATAR_GRAVATAR_DEFAULT
 
 register = template.Library()
 
@@ -29,9 +29,12 @@ def avatar_url(user, size=80):
         return avatar.avatar_url(size)
     else:
         if AVATAR_GRAVATAR_BACKUP:
+            params = {'s': str(size)}
+            if AVATAR_GRAVATAR_DEFAULT:
+                params['d'] = AVATAR_GRAVATAR_DEFAULT
             return "http://www.gravatar.com/avatar/%s/?%s" % (
                 md5_constructor(user.email).hexdigest(),
-                urllib.urlencode({'s': str(size)}),)
+                urllib.urlencode(params))
         else:
             return AVATAR_DEFAULT_URL
 register.simple_tag(avatar_url)
