67361f3933feccc25c4e107cc05f1e29872a85a7
Eric Florenzano <floguy@gmail.com>
2010-02-22 18:19:22 -0500

    Remove vestigial import

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -1,5 +1,3 @@
-import os.path
-
 from django.conf import settings
 
 try:
@@ -13,8 +11,7 @@ AVATAR_RESIZE_METHOD = getattr(settings, 'AVATAR_RESIZE_METHOD', Image.ANTIALIAS
 AVATAR_STORAGE_DIR = getattr(settings, 'AVATAR_STORAGE_DIR', 'avatars')
 AVATAR_GRAVATAR_BACKUP = getattr(settings, 'AVATAR_GRAVATAR_BACKUP', True)
 AVATAR_GRAVATAR_DEFAULT = getattr(settings, 'AVATAR_GRAVATAR_DEFAULT', None)
-AVATAR_DEFAULT_URL = getattr(settings, 'AVATAR_DEFAULT_URL', 
-    settings.MEDIA_URL + os.path.join(os.path.dirname(__file__), 'default.jpg'))
+AVATAR_DEFAULT_URL = getattr(settings, 'AVATAR_DEFAULT_URL', 'avatar/img/default.jpg')
 AVATAR_MAX_AVATARS_PER_USER = getattr(settings, 'AVATAR_MAX_AVATARS_PER_USER', 42)
 AVATAR_MAX_SIZE = getattr(settings, 'AVATAR_MAX_SIZE', 1024 * 1024)
 AVATAR_THUMB_FORMAT = getattr(settings, 'AVATAR_THUMB_FORMAT', "JPEG")
