8c1454222068e0f4960f52c3828f14ea9293bd47
Philipp Wassibauer <phil@maptales.com>
2010-09-06 00:45:11 -0700

    fixing simplefriend switch

--- avatar/views.py
+++ avatar/views.py
@@ -41,14 +41,17 @@ def _get_next(request):
     return next
     
 def _notification_updated(request, avatar):
-    notification.send([request.user], "avatar_updated",
-        {"user": request.user, "avatar": avatar})
-    if friends:
-        notification.send((x['friend'] for x in
+    try:
+        notification.send([request.user], "avatar_updated",
+            {"user": request.user, "avatar": avatar})
+        if friends:
+            notification.send((x['friend'] for x in
                 Friendship.objects.friends_for_user(request.user)),
-            "avatar_friend_updated",
-            {"user": request.user, "avatar": avatar}
-        )
+                "avatar_friend_updated",
+                {"user": request.user, "avatar": avatar}
+            )
+    except:
+        pass # in case simplefriends is used try not to crash to badly, in our case its in the activity stream anyway
 
 def _get_avatars(user):
     # Default set. Needs to be sliced, but that's it. Keep the natural order.
