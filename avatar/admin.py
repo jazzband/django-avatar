30d939ad70767ee52a43edeb3514589c32e21896
Philipp Wassibauer <phil@maptales.com>
2011-06-22 00:03:14 +0200

    admin interface displays avatar and is filterable

--- avatar/admin.py
+++ avatar/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from avatar.models import Avatar
+from templatetags.avatar_tags import avatar
 
-admin.site.register(Avatar)
+class AvatarAdmin(admin.ModelAdmin):
+    list_display        = ('get_avatar', 'user', 'primary', "date_uploaded")
+    list_filter         = ('primary',)
+    search_fields       = ('user__username',)
+    list_per_page       = 50
+    
+    def get_avatar(self, avatar_in):
+        return avatar(avatar_in.user, 80)
+        
+    get_avatar.short_description = 'Avatar'
+    get_avatar.allow_tags = True
+
+
+admin.site.register(Avatar, AvatarAdmin)
