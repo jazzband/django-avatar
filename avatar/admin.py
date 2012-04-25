59af1c3cb16e03b99543974c767b3e0291cd2ac3
Jannis Leidel <jannis@leidel.info>
2012-04-25 12:18:18 +0200

    Minor fixes for the admin class.

--- avatar/admin.py
+++ avatar/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
 from avatar.models import Avatar
-from templatetags.avatar_tags import avatar
+from avatar.templatetags.avatar_tags import avatar
+
 
 class AvatarAdmin(admin.ModelAdmin):
-    list_display        = ('get_avatar', 'user', 'primary', "date_uploaded")
-    list_filter         = ('primary',)
-    search_fields       = ('user__username',)
-    list_per_page       = 50
-    
+    list_display = ('get_avatar', 'user', 'primary', "date_uploaded")
+    list_filter = ('primary',)
+    search_fields = ('user__username',)
+    list_per_page = 50
+
     def get_avatar(self, avatar_in):
         return avatar(avatar_in.user, 80)
-        
-    get_avatar.short_description = 'Avatar'
-    get_avatar.allow_tags = True
 
+    get_avatar.short_description = _('Avatar')
+    get_avatar.allow_tags = True
 
 admin.site.register(Avatar, AvatarAdmin)
