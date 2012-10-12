22df7a89020cbbcf80a88bcf3572dea591884861
Cristiano Valente <cristiano.valente@gmail.com>
2012-10-12 14:45:47 +0200

    Support for username with extra chars.

--- avatar/urls.py
+++ avatar/urls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('avatar.views',
     url('^add/$', 'add', name='avatar_add'),
     url('^change/$', 'change', name='avatar_change'),
     url('^delete/$', 'delete', name='avatar_delete'),
-    url('^render_primary/(?P<user>[\+\w]+)/(?P<size>[\d]+)/$', 'render_primary', name='avatar_render_primary'),    
+    url('^render_primary/(?P<user>[\w\d\.\-_]{3,30})/(?P<size>[\d]+)/$', 'render_primary', name='avatar_render_primary'),    
 )
