ba38663babcd94dc1b3d3f83ebf1649fb5a3a1c1
andrean <andrean.franc@gmail.com>
2013-06-26 10:54:29 +0200

    dispatch avatar_updated signal from admin as well

--- avatar/admin.py
+++ avatar/admin.py
@@ -3,7 +3,9 @@ from django.utils.translation import ugettext_lazy as _
 
 from avatar.models import Avatar
 from avatar.templatetags.avatar_tags import avatar
+from avatar.signals import avatar_updated
 from avatar.util import User
+
 
 class AvatarAdmin(admin.ModelAdmin):
     list_display = ('get_avatar', 'user', 'primary', "date_uploaded")
@@ -16,5 +18,10 @@ class AvatarAdmin(admin.ModelAdmin):
 
     get_avatar.short_description = _('Avatar')
     get_avatar.allow_tags = True
+
+    def save_model(self, request, obj, form, change):
+        super(AvatarAdmin, self).save_model(request, obj, form, change)
+        avatar_updated.send(sender=Avatar, user=request.user, avatar=obj)
+
 
 admin.site.register(Avatar, AvatarAdmin)
