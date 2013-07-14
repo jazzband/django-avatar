cd9c9e10cb7130aed6d3203e17e8b542e90f50c7
Tomaz Muraus <tomaz@tomaz.me>
2013-07-14 17:40:17 +0200

    Update url configs.
    django.conf.urls.defaults has been deprecated and now fully removed in 1.6.

--- avatar/urls.py
+++ avatar/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    # Django < 1.4
+    from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'avatar.views',
