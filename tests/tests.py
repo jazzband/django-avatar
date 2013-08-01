39ecc33ced05b509fabf4ac4aac87feb6580a022
Jannis Leidel <jannis@leidel.info>
2013-08-01 13:33:14 +0200

    Use new test method names.

--- tests/tests.py
+++ tests/tests.py
@@ -30,37 +30,37 @@ class AvatarUploadTests(TestCase):
 
     def testNonImageUpload(self):
         response = upload_helper(self, "nonimagefile")
-        self.failUnlessEqual(response.status_code, 200)
-        self.failIfEqual(response.context['upload_avatar_form'].errors, {})
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.context['upload_avatar_form'].errors, {})
 
     def testNormalImageUpload(self):
         response = upload_helper(self, "test.png")
-        self.failUnlessEqual(response.status_code, 200)
-        self.failUnlessEqual(len(response.redirect_chain), 1)
-        self.failUnlessEqual(response.context['upload_avatar_form'].errors, {})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.context['upload_avatar_form'].errors, {})
         avatar = get_primary_avatar(self.user)
-        self.failIfEqual(avatar, None)
+        self.assertNotEqual(avatar, None)
 
     def testImageWithoutExtension(self):
         # use with AVATAR_ALLOWED_FILE_EXTS = ('.jpg', '.png')
         response = upload_helper(self, "imagefilewithoutext")
-        self.failUnlessEqual(response.status_code, 200)
-        self.failUnlessEqual(len(response.redirect_chain), 0)  # Redirect only if it worked
-        self.failIfEqual(response.context['upload_avatar_form'].errors, {})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 0)  # Redirect only if it worked
+        self.assertNotEqual(response.context['upload_avatar_form'].errors, {})
 
     def testImageWithWrongExtension(self):
         # use with AVATAR_ALLOWED_FILE_EXTS = ('.jpg', '.png')
         response = upload_helper(self, "imagefilewithwrongext.ogg")
-        self.failUnlessEqual(response.status_code, 200)
-        self.failUnlessEqual(len(response.redirect_chain), 0)  # Redirect only if it worked
-        self.failIfEqual(response.context['upload_avatar_form'].errors, {})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 0)  # Redirect only if it worked
+        self.assertNotEqual(response.context['upload_avatar_form'].errors, {})
 
     def testImageTooBig(self):
         # use with AVATAR_MAX_SIZE = 1024 * 1024
         response = upload_helper(self, "testbig.png")
-        self.failUnlessEqual(response.status_code, 200)
-        self.failUnlessEqual(len(response.redirect_chain), 0)  # Redirect only if it worked
-        self.failIfEqual(response.context['upload_avatar_form'].errors, {})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 0)  # Redirect only if it worked
+        self.assertNotEqual(response.context['upload_avatar_form'].errors, {})
 
     def testDefaultUrl(self):
         response = self.client.get(reverse('avatar_render_primary', kwargs={
@@ -76,25 +76,25 @@ class AvatarUploadTests(TestCase):
 
     def testNonExistingUser(self):
         a = get_primary_avatar("nonexistinguser")
-        self.failUnlessEqual(a, None)
+        self.assertEqual(a, None)
 
     def testThereCanBeOnlyOnePrimaryAvatar(self):
         for i in range(1, 10):
             self.testNormalImageUpload()
         count = Avatar.objects.filter(user=self.user, primary=True).count()
-        self.failUnlessEqual(count, 1)
+        self.assertEqual(count, 1)
 
     def testDeleteAvatar(self):
         self.testNormalImageUpload()
         avatar = Avatar.objects.filter(user=self.user)
-        self.failUnlessEqual(len(avatar), 1)
+        self.assertEqual(len(avatar), 1)
         response = self.client.post(reverse('avatar_delete'), {
             'choices': [avatar[0].id],
         }, follow=True)
-        self.failUnlessEqual(response.status_code, 200)
-        self.failUnlessEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 1)
         count = Avatar.objects.filter(user=self.user).count()
-        self.failUnlessEqual(count, 0)
+        self.assertEqual(count, 0)
 
     def testDeletePrimaryAvatarAndNewPrimary(self):
         self.testThereCanBeOnlyOnePrimaryAvatar()
@@ -104,10 +104,10 @@ class AvatarUploadTests(TestCase):
             'choices': [oid],
         })
         primaries = Avatar.objects.filter(user=self.user, primary=True)
-        self.failUnlessEqual(len(primaries), 1)
-        self.failIfEqual(oid, primaries[0].id)
+        self.assertEqual(len(primaries), 1)
+        self.assertNotEqual(oid, primaries[0].id)
         avatars = Avatar.objects.filter(user=self.user)
-        self.failUnlessEqual(avatars[0].id, primaries[0].id)
+        self.assertEqual(avatars[0].id, primaries[0].id)
 
     def testTooManyAvatars(self):
         for i in range(0, AVATAR_MAX_AVATARS_PER_USER):
@@ -115,10 +115,10 @@ class AvatarUploadTests(TestCase):
         count_before = Avatar.objects.filter(user=self.user).count()
         response = upload_helper(self, "test.png")
         count_after = Avatar.objects.filter(user=self.user).count()
-        self.failUnlessEqual(response.status_code, 200)
-        self.failUnlessEqual(len(response.redirect_chain), 0)  # Redirect only if it worked
-        self.failIfEqual(response.context['upload_avatar_form'].errors, {})
-        self.failUnlessEqual(count_before, count_after)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 0)  # Redirect only if it worked
+        self.assertNotEqual(response.context['upload_avatar_form'].errors, {})
+        self.assertEqual(count_before, count_after)
 
     # def testAvatarOrder
     # def testReplaceAvatarWhenMaxIsOne
