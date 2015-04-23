3d48f181f90995bd66dc436acccde9d18c5cfa3c
Grant McConnaughey <grantmcconnaughey@gmail.com>
2015-04-23 15:10:25 -0500

    Remove django.contrib.comments and add MIDDLEWARE_CLASSES

--- tests/settings.py
+++ tests/settings.py
@@ -14,9 +14,18 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sites',
-    'django.contrib.comments',
     'avatar',
 ]
+
+MIDDLEWARE_CLASSES = (
+    "django.middleware.common.BrokenLinkEmailsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+)
+
 
 ROOT_URLCONF = 'tests.urls'
 
