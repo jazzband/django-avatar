839220c51bfa17cd1683c86e6f7c6fab4528ec64
Grant McConnaughey <grantmcconnaughey@gmail.com>
2016-09-03 10:47:01 -0500

    Add test project for debugging

---
+++ test_proj/test_proj/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.static import serve
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^avatar/', include('avatar.urls')),
+]
+
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT})
+    ]
