9e1b674567c3b8c5e795d2cd58a2a0c7177d6999
Aaron Jorgensen <62412972+ajeema@users.noreply.github.com>
2022-03-15 08:24:10 -0500

    Update urls.py

--- avatar/urls.py
+++ avatar/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import include, re_path
 
 from avatar import views
 
 urlpatterns = [
-    url(r'^add/$', views.add, name='avatar_add'),
-    url(r'^change/$', views.change, name='avatar_change'),
-    url(r'^delete/$', views.delete, name='avatar_delete'),
-    url(r'^render_primary/(?P<user>[\w\d\@\.\-_]+)/(?P<size>[\d]+)/$',
+    re_path(r'^add/$', views.add, name='avatar_add'),
+    re_path(r'^change/$', views.change, name='avatar_change'),
+    re_path(r'^delete/$', views.delete, name='avatar_delete'),
+    re_path(r'^render_primary/(?P<user>[\w\d\@\.\-_]+)/(?P<size>[\d]+)/$',
         views.render_primary,
         name='avatar_render_primary'),
 ]
