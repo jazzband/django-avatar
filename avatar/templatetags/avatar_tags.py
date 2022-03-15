d749313817430be30165582f40e5edadf631dab7
Aaron Jorgensen <62412972+ajeema@users.noreply.github.com>
2022-03-15 08:31:44 -0500

    Update avatar_tags.py

--- avatar/templatetags/avatar_tags.py
+++ avatar/templatetags/avatar_tags.py
@@ -10,7 +10,7 @@ try:
     from django.utils import six
 except ImportError:
     import six
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 from django.utils.module_loading import import_string
 
 from avatar.conf import settings
