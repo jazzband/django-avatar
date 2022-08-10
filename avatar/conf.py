3e12122a9c2c35e555465b589f7c9061a4cdecc8
Johannes Wilm <mail@johanneswilm.org>
2022-08-10 17:24:00 +0200

    make default for AVATAR_EXPOSE_USERNAMES False. Relates to #181

--- avatar/conf.py
+++ avatar/conf.py
@@ -19,7 +19,7 @@ class AvatarConf(AppConf):
     THUMB_QUALITY = 85
     HASH_FILENAMES = False
     HASH_USERDIRNAMES = False
-    EXPOSE_USERNAMES = True
+    EXPOSE_USERNAMES = False
     ALLOWED_FILE_EXTS = None
     CACHE_TIMEOUT = 60 * 60
     STORAGE = settings.DEFAULT_FILE_STORAGE
