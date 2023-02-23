5025be283aa89779e2694c32cccf7916ebaaff5d
Johannes Wilm <mail@johanneswilm.org>
2023-02-24 00:08:07 +0100

    switch to setuptools, fixes #227

--- setup.py
+++ setup.py
@@ -20,35 +20,6 @@ def find_version(*file_paths):
 
 
 setup(
-    name="django-avatar",
-    version=find_version("avatar", "__init__.py"),
-    description="A Django app for handling user avatars",
-    long_description=read("README.rst"),
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "Framework :: Django",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.0",
-        "Framework :: Django :: 4.1",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    keywords="avatar, django",
-    author="Eric Florenzano",
-    author_email="floguy@gmail.com",
-    maintainer="Johannes Wilm",
-    maintainer_email="johannes@fiduswriter.org",
-    url="https://github.com/jazzband/django-avatar/",
-    license="BSD",
     packages=find_packages(exclude=["tests"]),
     package_data={
         "avatar": [
@@ -58,10 +29,5 @@ setup(
             "media/avatar/img/default.jpg",
         ],
     },
-    install_requires=[
-        "Pillow>=8.4.0",
-        "django-appconf>=1.0.5",
-        "dnspython>=2.3.0",
-    ],
     zip_safe=False,
 )
