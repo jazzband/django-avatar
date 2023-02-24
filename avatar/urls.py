f4208dbcb8207157dbea0274814290890b18e4df
0xMRTT <0xMRTT@proton.me>
2023-02-24 14:31:17 +0100

    fix(urls): url names

--- avatar/urls.py
+++ avatar/urls.py
@@ -7,18 +7,18 @@ from avatar import views
 app_name = "avatar"
 
 urlpatterns = [
-    path("add/", views.add, name="avatar_add"),
-    path("change/", views.change, name="avatar_change"),
-    path("delete/", views.delete, name="avatar_delete"),
+    path("add/", views.add, name="add"),
+    path("change/", views.change, name="change"),
+    path("delete/", views.delete, name="delete"),
     # https://docs.djangoproject.com/en/4.1/topics/http/urls/#path-converters
     path(
         "render_primary/<slug:user>/<int:width>/",
         views.render_primary,
-        name="avatar_render_primary",
+        name="render_primary",
     ),
     path(
         "render_primary/<slug:user>/<int:width>/<int:height>/",
         views.render_primary,
-        name="avatar_render_primary",
+        name="render_primary",
     ),
 ]
